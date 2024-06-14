7eb61d0f7f85310f9765ca96c6a86b361a12facb
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-06-14 09:46:11 -0400

    first commit

---
+++ app/src/main/java/com/example/healthyfind/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.healthyfind.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Welcome to Healthy Finds, this text will likely change :)");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
