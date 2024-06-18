83531fa58314515b480a6b97a96c65fb4b2b969c
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-06-18 16:42:46 -0400

    Started welcome page. Needed to add this to code so that top action bar would show up correctly. Page is still in progress.

---
+++ app/src/main/java/com/example/healthyfind/welcome/WelcomeFragment.java
@@ -0,0 +1,40 @@
+package com.example.healthyfind.welcome;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.healthyfind.databinding.FragmentWelcomeBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WelcomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        WelcomeViewModel welcomeViewModel =
+                new ViewModelProvider(this).get(WelcomeViewModel.class);
+
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- app/src/main/java/com/example/healthyfind/welcome/WelcomeViewModel.java
+++ app/src/main/java/com/example/healthyfind/welcome/WelcomeViewModel.java
@@ -1,14 +1,14 @@
-package com.example.healthyfind.ui.dashboard;
+package com.example.healthyfind.welcome;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class WelcomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public WelcomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment template");
     }
