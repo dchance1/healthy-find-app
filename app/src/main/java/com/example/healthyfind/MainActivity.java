354795449cf985f1398ec83261eb5275eb603f51
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-06-18 20:13:12 -0400

    Added maps page and nav icon. Added temp page for maps implementation.

--- app/src/main/java/com/example/healthyfind/MainActivity.java
+++ app/src/main/java/com/example/healthyfind/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_recipes, R.id.navigation_settings,R.id.navigation_welcome)
+                R.id.navigation_home, R.id.navigation_recipes, R.id.navigation_settings,R.id.navigation_maps)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
