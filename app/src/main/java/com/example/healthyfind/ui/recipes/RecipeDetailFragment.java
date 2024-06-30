a4eabc63a615f8f230431b4367a0c1709072a9f2
Publio (Leo) Samayoa <psamayoa@mail.valenciacollege.edu>
2024-06-29 23:35:18 -0400

    Adding Recipe Details Fragment and xml to view recipe details

--- app/src/main/java/com/example/healthyfind/ui/recipes/RecipeDetailFragment.java
+++ app/src/main/java/com/example/healthyfind/ui/recipes/RecipeDetailFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.bumptech.glide.Glide;
 import com.example.healthyfind.R;
@@ -26,6 +29,7 @@ public class RecipeDetailFragment extends Fragment {
         TextView recipeDescription = view.findViewById(R.id.recipe_description);
         TextView recipeIngredients = view.findViewById(R.id.recipe_ingredients);
         TextView recipeSteps = view.findViewById(R.id.recipe_steps);
+        Button closeButton = view.findViewById(R.id.close_button);
 
         if (getArguments() != null) {
             RecipesFragment.Meal meal = (RecipesFragment.Meal) getArguments().getSerializable("meal");
@@ -37,6 +41,16 @@ public class RecipeDetailFragment extends Fragment {
                 recipeSteps.setText(meal.getInstructions());
             }
         }
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                fragmentManager.beginTransaction()
+                        .remove(RecipeDetailFragment.this)
+                        .commit();
+            }
+        });
 
         return view;
     }
